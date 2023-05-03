6778377c551025f7c25730974004f8a29b2b5a55
Fernando <beratechsolutions@gmail.com>
2023-05-03 02:18:35 -0300

    inserindo animacoes em javascript ao projeto

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,14 @@
+function handleMouseEnter() {
+    this.classList.add('s-card--hovered');
+}
+
+function addEventListenersToCards() {
+    const cardElements = document.getElementsByClassName('s-card');
+    
+    for (let index = 0; index < cardElements.length; index++) {
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
